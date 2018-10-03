f6747f97293e19075cebb0d670015e834eb641d7
ragha <ragha@ragha.com>
2018-10-02 21:10:54 -0500

    Added deploy script

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+docker build -t raghavendranataraj/multi-client:latest -t raghavendra/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t raghavendranataraj/multi-server:latest -t raghavendra/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t raghavendranataraj/multi-worker:latest -t raghavendra/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push raghavendranataraj/multi-client:latest
+docker push raghavendranataraj/multi-server:latest
+docker push raghavendranataraj/multi-worker:latest
+docker push raghavendranataraj/multi-client:$SHA
+docker push raghavendranataraj/multi-server:$SHA
+docker push raghavendranataraj/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=raghavendranataraj/multi-server:$SHA
+kubectl set image deployments/client-deployment client=raghavendranataraj/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=raghavendranataraj/multi-worker:$SHA
